80e2467af0fc8ecf5c44286447c121ec43bfb434
Aditya Agarwal <adityaa803@gmail.com>
2020-06-06 17:15:37 +0530

    Initial App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,99 @@
-import React from "react";
-import "./styles.css";
+import React, { useState } from 'react';
+import cn from 'clsx';
+
+import { housesData, charactersData } from './staticData';
+import './styles.css';
+
+function Header(props) {
+  const { children } = props;
+  return (
+    <header className="app-header">
+      <h1 className="app-name">Potter World</h1>
+      {children}
+    </header>
+  );
+}
+
+function Picker(props) {
+  const { activeHouseId, onChange } = props;
+  return (
+    <div className="house-picker">
+      <h3>Filter by House</h3>
+      <ul className="house-list">
+        {housesData.map(house => (
+          <li>
+            <button
+              className={cn('house', activeHouseId === house.id && 'active')}
+              onClick={event => onChange(house.id, event)}
+            >
+              <span>{house.name}</span>
+              <img
+                src={house.imageUrl}
+                alt={house.name}
+                className="house-logo"
+              />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Table(props) {
+  const { charactersList } = props;
+  return (
+    <table className="table">
+      <thead className="table-head">
+        <tr className="table-row">
+          <th className="table-cell">Name</th>
+          <th className="table-cell">House</th>
+        </tr>
+      </thead>
+      <tbody>
+        {charactersList.map(character => (
+          <tr>
+            <td className="table-cell">{character.name}</td>
+            <td className="table-cell">{character.house}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 function App() {
+  const [stateTheme, setStateTheme] = useState('dark');
+  const [stateActiveHouseId, setStateActiveHouseId] = useState('all');
+
+  function filterByHouse(houseId) {
+    if (houseId === 'all') {
+      return charactersData;
+    }
+    return charactersData.filter(character => character.houseId === houseId);
+  }
+
+  function handleChangeTheme() {
+    setStateTheme(oldTheme => (oldTheme === 'dark' ? 'light' : 'dark'));
+  }
+
+  function handleFilter(houseId) {
+    setStateActiveHouseId(houseId);
+  }
+
+  const filteredCharacters = filterByHouse(stateActiveHouseId);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className={cn('app', `theme-${stateTheme}`)}>
+      <Header>
+        <button className="theme-switch-btn" onClick={handleChangeTheme}>
+          {stateTheme === 'light' ? 'Nox' : 'Lumos'}
+        </button>
+      </Header>
+      <main>
+        <Picker activeHouseId={stateActiveHouseId} onChange={handleFilter} />
+        <Table charactersList={filteredCharacters} />
+      </main>
     </div>
   );
 }
---
+++ src/staticData.js
@@ -0,0 +1,80 @@
+export const housesData = [
+  {
+    id: 'all',
+    name: 'All',
+    imageUrl: '/hogwarts.webp',
+  },
+  {
+    id: 'gryffindor',
+    name: 'Gryffindor',
+    imageUrl: '/gryffindor.webp',
+  },
+  {
+    id: 'hufflepuff',
+    name: 'Hufflepuff',
+    imageUrl: '/hufflepuff.webp',
+  },
+  {
+    id: 'slytherin',
+    name: 'Slytherin',
+    imageUrl: '/slytherin.webp',
+  },
+  {
+    id: 'ravenclaw',
+    name: 'Ravenclaw',
+    imageUrl: '/ravenclaw.webp',
+  },
+];
+
+export const charactersData = [
+  {
+    name: 'Harry Potter',
+    house: 'Gryffindor',
+    houseId: 'gryffindor',
+  },
+  {
+    name: 'Cedric Diggory',
+    house: 'Hufflepuff',
+    houseId: 'hufflepuff',
+  },
+  {
+    name: 'Luna Lovegood',
+    house: 'Ravenclaw',
+    houseId: 'ravenclaw',
+  },
+  {
+    name: 'Severus Snape',
+    house: 'Slytherin',
+    houseId: 'slytherin',
+  },
+  {
+    name: 'Ron Weasley',
+    house: 'Gryffindor',
+    houseId: 'gryffindor',
+  },
+  {
+    name: 'Hermione Granger',
+    house: 'Gryffindor',
+    houseId: 'gryffindor',
+  },
+  {
+    name: 'Gilderoy Lockhart',
+    house: 'Ravenclaw',
+    houseId: 'ravenclaw',
+  },
+  {
+    name: 'Draco Malfoy',
+    house: 'Slytherin',
+    houseId: 'slytherin',
+  },
+  {
+    name: 'Newt Scamander',
+    house: 'Hufflepuff',
+    houseId: 'hufflepuff',
+  },
+  {
+    name: 'Tom Riddle',
+    house: 'Slytherin',
+    houseId: 'slytherin',
+  },
+];
